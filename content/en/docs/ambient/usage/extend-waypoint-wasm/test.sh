f5bf98ff0008c95904faafd56b6d9e09b962f5a8
Adil Mohamed <adilmohamedmp1@gmail.com>
2024-05-14 15:58:01 +0530

    completed test.sh

--- content/en/docs/ambient/usage/extend-waypoint-wasm/test.sh
+++ content/en/docs/ambient/usage/extend-waypoint-wasm/test.sh
@@ -49,10 +49,56 @@ _verify_contains snip_verify_traffic_notsleep_to_productpage "$snip_verify_traff
 
 _verify_same snip_adding_your_application_to_the_ambient_mesh_1 "$snip_adding_your_application_to_the_ambient_mesh_1_out"
 
-# test traffic after ambient mode is enabled
+# ambient mode enabled
 snip_adding_your_application_to_the_ambient_mesh_2
+
+# test traffic after ambient mode is enabled
 _verify_contains snip_adding_your_application_to_the_ambient_mesh_3 "$snip_adding_your_application_to_the_ambient_mesh_3_out"
 _verify_same snip_adding_your_application_to_the_ambient_mesh_4 "$snip_adding_your_application_to_the_ambient_mesh_4_out"
+
+# Display existing gateways and verify output
+_verify_like snip_configure_wasmplugin_for_gateway_1 "$snip_configure_wasmplugin_for_gateway_1_out"
+
+# Configure WASM plugin for gateway
+snip_configure_wasmplugin_for_gateway_2
+
+# verify traffic via gateway
+_verify_same snip_verify_the_traffic_via_the_gateway_1 "$snip_verify_the_traffic_via_the_gateway_1_out"
+_verify_same snip_verify_the_traffic_via_the_gateway_2 "$snip_verify_the_traffic_via_the_gateway_2_out"
+
+# Deploy a waypoint proxy
+snip_deploy_a_waypoint_proxy_1
+
+# verify traffic_without wasmplugin at the waypoint
+_verify_same snip_verify_traffic_without_wasmplugin_at_the_waypoint_1 "$snip_verify_traffic_without_wasmplugin_at_the_waypoint_1_out"
+
+# Display existing gateways and verify output
+_verify_like snip_apply_wasmplugin_at_waypoint_proxy_1 "$snip_apply_wasmplugin_at_waypoint_proxy_1_out"
+
+# apply wasmplugin at waypoint proxy
+snip_apply_wasmplugin_at_waypoint_proxy_2
+
+# Display applied wasmplugins and verify output
+_verify_like snip_view_the_configured_wasmplugin_1 "$snip_view_the_configured_wasmplugin_1_out"
+
+# verify the traffic via waypoint proxy
+_verify_same snip_verify_the_traffic_via_waypoint_proxy_1 "$snip_verify_the_traffic_via_waypoint_proxy_1_out"
+_verify_same snip_verify_the_traffic_via_waypoint_proxy_2 "$snip_verify_the_traffic_via_waypoint_proxy_2_out"
+
+# Apply Waypoint configuration for 'reviews-svc' service
+_verify_same snip_apply_wasmplugin_for_a_specific_service_using_waypoint_1 "$snip_apply_wasmplugin_for_a_specific_service_using_waypoint_1_out"
+_verify_same snip_apply_wasmplugin_for_a_specific_service_using_waypoint_2 "$snip_apply_wasmplugin_for_a_specific_service_using_waypoint_2_out"
+
+# Display existing gateways and verify output
+_verify_like snip_apply_wasmplugin_for_a_specific_service_using_waypoint_3 "$snip_apply_wasmplugin_for_a_specific_service_using_waypoint_3_out"
+
+# apply wasmplugin for a specific service using waypoint
+snip_apply_wasmplugin_for_a_specific_service_using_waypoint_4
+
+# verify the traffic targeting the service
+_verify_same snip_verify_the_traffic_targeting_the_service_1 "$snip_verify_the_traffic_targeting_the_service_1_out"
+_verify_same snip_verify_the_traffic_targeting_the_service_2 "$snip_verify_the_traffic_targeting_the_service_2_out"
+_verify_same snip_verify_the_traffic_targeting_the_service_3 "$snip_verify_the_traffic_targeting_the_service_3_out"
 
 # @cleanup
 snip_uninstall_1
